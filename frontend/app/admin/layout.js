4723b9518ed150ee4c9fbef82bb7f2616ecf2da1
Sarthak Sahu <70280920+sarthak5461@users.noreply.github.com>
2026-09-22 16:16:34 +0530

    Delete frontend directory

--- frontend/app/admin/layout.js
+++
@@ -1,8 +0,0 @@
-import "./admin.css";
-import AdminShell from "../../components/admin/AdminShell";
-
-export const metadata = { title: "Admin | GrowPlus+" };
-
-export default function AdminLayout({ children }) {
-  return <AdminShell>{children}</AdminShell>;
-}

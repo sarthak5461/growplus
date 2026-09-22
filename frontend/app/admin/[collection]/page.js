00d6553e2b3216eb2f2e911ca802c28c4dde2423
Sarthak Sahu <sarthaksahu30@gmail.com>
2026-09-22 16:17:08 +0530

    updated

--- frontend/app/admin/[collection]/page.js
+++
@@ -1,9 +0,0 @@
-import { notFound } from "next/navigation";
-import CollectionEditor from "../../../components/admin/CollectionEditor";
-import { FIELDS, TITLES } from "../../../lib/adminFields";
-
-export default async function CollectionPage({ params }) {
-  const { collection } = await params;
-  if (!FIELDS[collection]) notFound();
-  return <CollectionEditor collection={collection} title={TITLES[collection]} />;
-}
--- frontend/app/affiliate/page.js
+++
@@ -1,9 +0,0 @@
-import ServiceDetail from "../../components/ServiceDetail";
-import { getSiteContent } from "../../lib/cms";
-
-export const metadata = { title: "Affiliate Marketing | GrowPlus+" };
-
-export default async function Page() {
-  const { services } = await getSiteContent();
-  return <ServiceDetail service={services.find((s) => s.slug === "affiliate")} />;
-}
